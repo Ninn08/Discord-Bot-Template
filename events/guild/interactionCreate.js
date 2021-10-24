4fbece81f884cce4ce11979964a5cda6189f3dd5
Nin <darshan310268@gmail.com>
2021-10-24 09:05:08 +0530

    command & menu

--- events/guild/interactionCreate.js
+++ events/guild/interactionCreate.js
@@ -1,17 +1,39 @@
-module.exports = async (client, interaction) => {
-    if (interaction.isCommand()) {
-      const command = client.slashCommands.get(interaction.commandName);
-      if (!command) return;
+const { Client, Interaction, CommandInteraction, ContextMenuInteraction } = require("discord.js");
 
-      // execute
-      try {
-        await command.execute(client, interaction)
-      } catch (error) {
-        console.log(error)
-        await interaction.reply({
-          content: "An error occurred while executing the command",
-          ephemeral: true
-        }).catch(console.error)
-      }
+/**
+ * 
+ * @param {Client} client 
+ * @param {Interaction|CommandInteraction|ContextMenuInteraction} interaction 
+ */
+
+module.exports = async (client, interaction) => {
+  if(interaction.isCommand()){
+    const command = client.slashCommands.get(interaction.commandName);
+    if(!command) return;
+
+    try {
+      await command.execute(client, interaction)
+    } catch (error) {
+      console.log(error)
+      await interaction.reply({
+        content: `An error occurred`,
+        ephemeral: true
+      })
     }
-};
+  }
+
+  if(interaction.isContextMenu()){
+    const command = client.menuCommands.get(interaction.commandName)
+    if(!command) return;
+
+    try {
+      await command.execute(client, interaction)
+    } catch (error) {
+      console.log(error)
+      await interaction.reply({
+        content: `An error occurred`,
+        ephemeral: true
+      })
+    }
+  }
+}
